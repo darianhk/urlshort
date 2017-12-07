4d2a86e5c3798ae3dea58e54918e3c6e267f0c06
Glitch (url-shrt) <none>
2017-12-07 22:01:46 +0000

    🎓🎅 Updated with Glitch

--- server.js
+++ server.js
@@ -3,35 +3,47 @@ var express = require('express');
 var shortid = require('shortid');
 var mongoclient = require('mongodb').MongoClient();
 var app = express();
-var appurl = "https://sour-request.glitch.me/";
+var appurl = "https://url-shrt.glitch.me/id/";
+var database;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // take in full url and generate shortened url
 mongoclient.connect("mongodb://darianhk:ettinfreak9@ds113046.mlab.com:13046/urlshrtdhk", function(err, db){
+  database = db;
+})
+  
+  app.get('/', function (req, res){
+  res.sendFile(__dirname + '/views/index.html')
+})
+  
+  
+
+                      
 app.get("/new/:longURL(*)", function (req, res) {
   var long = req.params.longURL;
   var id = shortid.generate().slice(0,6);
   var short = appurl + id;
   res.json({original: long, shortened: short});
-  db.collection('urlshort').insert({
+  database.collection('urlshort').insert({
     original: long,
     shortened: short,
     id: id
   })
+  console.log("url generated")
 })
 
+  
 // redirect to original url based on id
-app.get('/:id(*)', function(req, res){
+app.get('/id/:id', function(req, res){
   var url = "";
-  var obj = db.collection('urlshort').find({
+  var obj = database.collection('urlshort').find({
     id: req.params.id
   }).toArray(function(err, data){
-    url = (data[0].original);
-    res.redirect(url)
+    res.redirect(data[0].original)
   });
-})
+  console.log("redirecting")
 })
 
 // listen for requests :)
